4985ce8a004367a0e0c35764432eacd0374c7103
yaahor <yaahor@gmail.com>
2025-03-08 13:17:17 +0100

    pages: offboarding: add offboarding VO

---
+++ src/pages/offboarding/offboarding.vo.ts
@@ -0,0 +1,18 @@
+import { Status } from '../../shared/model/status';
+import { User } from '../../shared/model/user';
+
+export type OffboardingVo = OffboardingErrorVo | OffboardingLoadingVo | OffboardingSuccessVo;
+
+
+export interface OffboardingErrorVo {
+  status: Status.ERROR;
+}
+
+export interface OffboardingLoadingVo {
+  status: Status.LOADING;
+}
+
+export interface OffboardingSuccessVo {
+  status: Status.SUCCESS;
+  users: User[];
+}
