70738b539cf1151f218483858fe4c2831bf11214
yaahor <yaahor@gmail.com>
2025-03-08 15:17:23 +0100

    pages: offboarding: display users

--- src/pages/offboarding/offboarding.component.ts
+++ src/pages/offboarding/offboarding.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Observable } from 'rxjs';
+import { Status } from '../../shared/model/status';
 import { OffboardingService } from './offboarding.service';
 import { OffboardingVo } from './model/offboarding.vo';
 
 @Component({
   selector: 'app-offboarding',
-  imports: [],
+  imports: [MatProgressSpinnerModule, CommonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './offboarding.component.html',
   styleUrl: './offboarding.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,6 +20,7 @@ import { OffboardingVo } from './model/offboarding.vo';
 })
 export class OffboardingComponent implements OnInit {
   protected vo$?: Observable<OffboardingVo>;
+  protected readonly Status = Status;
 
   constructor(private service: OffboardingService) {
 
