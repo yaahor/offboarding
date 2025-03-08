54a81455540a08f911367c01cf5f3e8752f4165d
yaahor <yaahor@gmail.com>
2025-03-08 23:16:11 +0100

    pages: employee-details: generate component

---
+++ src/pages/employee-details/employee-details.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-details',
+  imports: [],
+  templateUrl: './employee-details.component.html',
+  styleUrl: './employee-details.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EmployeeDetailsComponent {
+
+}
