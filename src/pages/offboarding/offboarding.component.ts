44e5bd45f6e66fcc640f5ebae416438d38b7d172
yaahor <yaahor@gmail.com>
2025-03-08 13:23:16 +0100

    pages: offboarding: set OnPush change detection

--- src/pages/offboarding/offboarding.component.ts
+++ src/pages/offboarding/offboarding.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-offboarding',
   imports: [],
   templateUrl: './offboarding.component.html',
-  styleUrl: './offboarding.component.scss'
+  styleUrl: './offboarding.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OffboardingComponent {
 
