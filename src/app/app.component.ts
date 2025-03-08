2b81afe81c7037025cd1769469dfa5dbd4bed57a
yaahor <yaahor@gmail.com>
2025-03-08 12:08:50 +0100

    app: add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
