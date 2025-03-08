c9bb699b3c4bb8b7ab304993337772b98d79d803
yaahor <yaahor@gmail.com>
2025-03-08 12:21:18 +0100

    app: add route to offboarding page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { OffboardingComponent } from '../pages/offboarding/offboarding.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: OffboardingComponent,
+  },
+  { path: '**', redirectTo: '', }
+];
