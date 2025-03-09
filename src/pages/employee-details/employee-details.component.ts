111c390fa9e69e8db813c567ec3e9c48ace3329a
yaahor <yaahor@gmail.com>
2025-03-09 21:00:48 +0100

    pages: employee-details: pass user to offboarding dialog

--- src/pages/employee-details/employee-details.component.ts
+++ src/pages/employee-details/employee-details.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Observable, switchMap } from 'rxjs';
 import { Equipment } from '../../entities/user/model/equipment';
+import { User } from '../../entities/user/model/user';
 import { UserStatus } from '../../entities/user/model/user-status';
 import { OffboardingDialogComponent } from '../../features/offboarding-dialog/offboarding-dialog.component';
 import { RouteParam } from '../../shared/model/route-param';
@@ -26,8 +27,7 @@ export class EmployeeDetailsComponent implements OnInit {
   protected readonly Status = Status;
   protected readonly UserStatus = UserStatus;
   protected vo$?: Observable<EmployeeDetailsVo>;
-
-  private dialog = inject(MatDialog);
+  private readonly dialog = inject(MatDialog);
 
   constructor(private readonly route: ActivatedRoute, private readonly service: EmployeeDetailsService) {
   }
@@ -47,7 +47,7 @@ export class EmployeeDetailsComponent implements OnInit {
     return equipment.id;
   }
 
-  protected onOffboardClick(): void {
-    this.dialog.open(OffboardingDialogComponent);
+  protected onOffboardClick(user: User): void {
+    this.dialog.open(OffboardingDialogComponent, { data: user, closeOnNavigation: true });
   }
 }
