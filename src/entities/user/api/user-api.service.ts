1d622e5f9986723428e43d1ae29a8ef228ca40f0
yaahor <yaahor@gmail.com>
2025-03-09 12:43:58 +0100

    entities: user: implement getUser method for fetching users by ID

--- src/entities/user/api/user-api.service.ts
+++ src/entities/user/api/user-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { delay, Observable, of } from 'rxjs';
+import { delay, Observable, of, throwError } from 'rxjs';
 import { mockUsers } from './mock-users';
 import { UserDto } from './user.dto';
 
@@ -12,8 +12,19 @@ export class UserApiService {
    * Retrieves a list of users.
    */
   getUsers(): Observable<UserDto[]> {
-    return of(mockUsers).pipe(
-      delay(2000),
-    );
+    return of(mockUsers).pipe(delay(2000),);
+  }
+
+  /**
+   * Retrieves a user by its id.
+   */
+  getUser(id: string): Observable<UserDto> {
+    const user = mockUsers.find(user => user.id === id);
+
+    if (!user) {
+      return throwError(() => new Error('User not found'));
+    }
+
+    return of(user).pipe(delay(2000));
   }
 }
