201325794f9ee2134ea55b03060727e6e0ebdde2
yaahor <yaahor@gmail.com>
2025-03-10 12:21:01 +0100

    entities: user: api: use mock service

--- src/entities/user/api/user-api.service.ts
+++ src/entities/user/api/user-api.service.ts
@@ -1,44 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay, Observable, of, throwError } from 'rxjs';
-import { UserStatus } from '../model/user-status';
-import { mockUsers } from '../../../mock/mock-users';
+import { Observable } from 'rxjs';
+import { mockServerPort } from '../../../shared/config/mock-server-port';
 import { OnboardingDto } from './onboarding.dto';
 import { UserDto } from './user.dto';
+
+const apiUrl = `http://localhost:${mockServerPort}`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserApiService {
+  constructor(private readonly httpClient: HttpClient) {
+  }
 
   /**
    * Retrieves a list of users.
    */
   getUsers(): Observable<UserDto[]> {
-    return of(mockUsers).pipe(delay(2000),);
+    return this.httpClient.get<UserDto[]>(`${apiUrl}/employees`);
   }
 
   /**
    * Retrieves a user by its id.
    */
   getUser(id: string): Observable<UserDto> {
-    const user = mockUsers.find(user => user.id === id);
-
-    if (!user) {
-      return throwError(() => new Error('User not found'));
-    }
-
-    return of(user).pipe(delay(2000));
+    return this.httpClient.get<UserDto>(`${apiUrl}/employees/${id}`);
   }
 
   conductOffboarding(userId: string, dto: OnboardingDto): Observable<void> {
-    const user = mockUsers.find(user => user.id === userId);
-
-    if (!user) {
-      return throwError(() => new Error('User not found'));
-    }
-
-    user.status = UserStatus.OFFBOARDED;
-
-    return of(void 0).pipe(delay(2000));
+    return this.httpClient.post<void>(`${apiUrl}/users/${userId}/offboard`, dto);
   }
 }
