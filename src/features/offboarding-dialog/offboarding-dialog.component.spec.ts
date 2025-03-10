2e30da91863de428751007a806fe4b96ba1cd3f3
yaahor <yaahor@gmail.com>
2025-03-10 17:13:27 +0100

    features: offboarding-dialog: fix test

--- src/features/offboarding-dialog/offboarding-dialog.component.spec.ts
+++ src/features/offboarding-dialog/offboarding-dialog.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 import { OffboardingDialogComponent } from './offboarding-dialog.component';
+import { OffboardingDialogService } from './offboarding-dialog.service';
 
-describe('OffboardingDialogComponent', () => {
+describe(OffboardingDialogComponent.name, () => {
   let component: OffboardingDialogComponent;
   let fixture: ComponentFixture<OffboardingDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [OffboardingDialogComponent]
+      imports: [OffboardingDialogComponent],
+      providers: [
+        { provide: OffboardingDialogService, useValue: {} },
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+      ],
     })
     .compileComponents();
 
--- src/features/offboarding-dialog/offboarding-dialog.service.spec.ts
+++ src/features/offboarding-dialog/offboarding-dialog.service.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { UserService } from '../../entities/user/model/user.service';
 import { OffboardingDialogService } from './offboarding-dialog.service';
 
 describe(OffboardingDialogService.name, () => {
   let service: OffboardingDialogService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: UserService, useValue: {} }],
+    });
     service = TestBed.inject(OffboardingDialogService);
   });
 
